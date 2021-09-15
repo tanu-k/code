f01d1d0940867d984da620b1e91ebb9953344ea3
tanu-k <tanushri.kabra@gmail.com>
2021-09-15 18:17:06 +0530

    up

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char const *argv[])
+{
+	
+    int n, n1, rev = 0, remainder;
+    
+    scanf("%d", &n);
+
+    n1 = n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n = n/10;
+    }
+
+    if(rev == n1) printf("YES\n");
+    else printf("NO\n");
+
+	return 0;
+}
