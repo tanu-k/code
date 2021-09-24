cc346e91cf72864b7dc8884d15b8909e8efa6fcf
tanu-k <tanushri.kabra@gmail.com>
2021-09-24 10:34:01 +0530

    Codeforces-281A

---
+++ word_capitalization.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	string s;
+
+	cin >> s;
+
+	s[0] = toupper(s[0]);
+
+	cout << s << endl;
+	
+	return 0;
+}
