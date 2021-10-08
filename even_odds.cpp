90a0b8617346bff7f902f33207c30627c73d4179
tanu-k <tanushri.kabra@gmail.com>
2021-10-08 17:28:57 +0530

    Codeforces-318A

---
+++ even_odds.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	long long n, k;
+
+	cin >> n >> k;
+
+	if(k <= (n + 1) / 2) {
+
+		cout << 2 * k - 1 << endl;
+	}
+
+	else cout << (k - (n + 1) / 2) * 2 << endl;
+
+	return 0;
+}
