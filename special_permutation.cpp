b3b2dc6987f57fce393167f86610388df38e240f
tanu-k <tanushri.kabra@gmail.com>
2021-11-22 21:58:05 +0530

    Codeforces-1612B

--- special_permutation.cpp
+++ special_permutation.cpp
@@ -63,30 +63,6 @@ int main(int argc, char const *argv[])
 
 			cout << endl;
 
-		// x = a;
-		// y = b;
-		// memset(arr, 0, sizeof(arr));
-
-		// if(n - a < n / 2 || n - b > n / 2) cout << - 1;
-		// else {
-		// 	for(i = 0; i < n / 2; i++) {
-		// 		if(x == b) continue;
-		// 		if(arr[x] == 0) cout << x << " ";
-		// 		arr[x] = 1;
-		// 		x += 1;
-		// 	}
-
-		// 	for(i = n / 2; i < n; i++) {
-		// 		if(y == a) continue;
-		// 		if(arr[y] == 0) {
-		// 			cout << y << " ";
-		// 			arr[y] = 1;
-		// 		}
-		// 		y -= 1;
-		// 	}
-		// } 
-
-		// cout << endl;
 	}
 	
 	return 0;
